fa5b6dfe902cd8ec5a6f0db9580cf630117edd62
Teste <teste@saleplus.net.br>
2022-11-21 20:26:34 -0300

    Alteração da Navbar de acordo com a autenticação do usuário.

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/logo.png'
 import styles from './Navbar.module.css'
+import { UserContext } from '../../context/UserContext'
 
 const Navbar = () => {
+    const { authenticated, logout } = useContext(UserContext)
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar_logo}>
@@ -11,8 +15,18 @@ const Navbar = () => {
             </div>
             <ul>
                 <li><Link to="/">Adotar</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Registrar</Link></li>
+                {
+                    authenticated ? (
+                        <>
+                            <li onClick={logout}>Sair</li>
+                        </>
+                    ) : (
+                        <>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Registrar</Link></li>
+                        </>
+                    )
+                }
             </ul>
         </nav>
     )
