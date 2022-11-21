44b17ab58c677af5a421415bffe09e5364e6ab88
Teste <teste@saleplus.net.br>
2022-11-21 00:07:33 -0300

    Criação das rotas Home, Login e Register.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Pages
+import Home from './components/pages/Home'
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
 
 function App() {
   return (
     <div className="App">
-      <h1>Get A Pet</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
