947cdc9a1afa9a18f216158c7699b48c810e897e
Teste <teste@saleplus.net.br>
2022-11-23 19:48:54 -0300

    Criação da página de adoções de usuário.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import MyPets from './components/pages/Pet/MyPets'
 import AddPet from './components/pages/Pet/AddPet'
 import EditPet from './components/pages/Pet/EditPet'
 import PetDetails from './components/pages/Pet/PetDetails'
+import MyAdoptions from './components/pages/Pet/MyAdoptions'
 
 // Components
 import Navbar from './components/layout/Navbar'
@@ -32,6 +33,7 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/user/profile' element={<Profile />} />
             <Route path='/pet/mypets' element={<MyPets />} />
+            <Route path='/pet/myadoptions' element={<MyAdoptions />} />
             <Route path='/pet/add' element={<AddPet />} />
             <Route path='/pet/edit/:id' element={<EditPet />} />
             <Route path='/pet/:id' element={<PetDetails />} />
