4e6ba18f6d6f7def773c1c404fd907f1de2f9aa4
Teste <teste@saleplus.net.br>
2022-11-23 19:09:37 -0300

    Criação da página de edição de pet.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/pages/Auth/Register'
 import Profile from './components/pages/User/Profile'
 import MyPets from './components/pages/Pet/MyPets'
 import AddPet from './components/pages/Pet/AddPet'
+import EditPet from './components/pages/Pet/EditPet'
 
 // Components
 import Navbar from './components/layout/Navbar'
@@ -31,6 +32,7 @@ function App() {
             <Route path='/user/profile' element={<Profile />} />
             <Route path='/pet/mypets' element={<MyPets />} />
             <Route path='/pet/add' element={<AddPet />} />
+            <Route path='/pet/edit/:id' element={<EditPet />} />
           </Routes>
         </Container>
         <Footer />
